bfbdc32db4f156839abd9936d002e2d44c22d6a5
literal_ys <2000pys@gmail.com>
2023-04-27 21:18:54 +0900

    initial commit

---
+++ roadmap/src/main/java/com/react/roadmap/RoadmapApplication.java
@@ -0,0 +1,13 @@
+package com.react.roadmap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RoadmapApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RoadmapApplication.class, args);
+    }
+
+}
---
+++ roadmap/src/test/java/com/react/roadmap/RoadmapApplicationTests.java
@@ -0,0 +1,13 @@
+package com.react.roadmap;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RoadmapApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
