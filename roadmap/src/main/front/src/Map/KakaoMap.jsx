4ab7e361cd0fb15f95970e522605fe3112d5d373
kibeom0806@gmail.com <kibeom0806@gmail.com>
2023-06-10 13:27:41 +0900

    카메라 이미지 추가

--- roadmap/src/main/front/src/Map/KakaoMap.jsx
+++ roadmap/src/main/front/src/Map/KakaoMap.jsx
@@ -136,12 +136,20 @@ function KakaoMap() {
 
                     if(imgChk(node)===true){
                         console.log("exist");
-                        const markerPosition = new window.kakao.maps.LatLng(
+                        const camera = "camera.png";
+
+                        const imageSrc = getImgAdd(camera), // 마커이미지의 주소입니다
+                            imageSize = new kakao.maps.Size(32, 35), // 마커이미지의 크기입니다
+                            imageOption = {offset: new kakao.maps.Point(14, 34)};
+
+                        const markerImage  = new kakao.maps.MarkerImage(imageSrc, imageSize, imageOption),
+                            markerPosition = new window.kakao.maps.LatLng(
                             parseFloat(dLatLng[i][0]),
                             parseFloat(dLatLng[i][1])
                         );
                         const newMarker = new window.kakao.maps.Marker({
                             position: markerPosition,
+                            image: markerImage
                         });
                         newMarker.setMap(map);
                         newMarkers.push(newMarker);
